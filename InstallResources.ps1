ceaf6af7719d2ceb563e76bd537c21865639ecc5
Michael Condon <michaelcondon@outlook.com>
2021-06-25 13:47:27 +1000

    Added Deploy script
    The deploy script moves the resources to a central location on C:/ to make it consistent to deploy this on multiple devices

---
+++ InstallResources.ps1
@@ -0,0 +1,14 @@
+Write-Output "This script is copying a series of scripts and templates for Sandbox to you C drive"
+Write-Output "This will create a folder on your Root C:\ directory titled Sandbox"
+
+#Set the current directory
+$dr = $PSScriptRoot + '\Data'
+#Establish the PATH for each file to move because PowerShell moving is annoying
+$ih = $dr + '\InstallHelper.cmd'
+$is = $dr + '\InstallStuff.ps1'
+$td = $dr + '\TeamsDemo.wsb'
+
+New-Item -Path "c:\" -Name "Sandbox" -ItemType "directory"
+Copy-Item -Path $ih -Destination C:\Sandbox
+Copy-Item -Path $is -Destination C:\Sandbox
+Copy-Item -Path $td -Destination C:\Sandbox
--- RunDemoSandbox.ps1
+++ RunDemoSandbox.ps1
@@ -3,18 +3,10 @@ Write-Output "Downloading Teams fresh from the web - this will take a few minute
 Write-Output "Sandbox Configuration by Michael Condon, contact micondon@microsoft.com for details"
 Write-Output " "
 Write-Output "Downloading Teams, please wait"
-Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/p/?LinkID=869426&clcid=0xc09&culture=en-au&country=AU&lm=deeplink&lmsrc=groupChatMarketingPageWeb&cmpid=directDownloadWin64" -OutFile "Data\Teams.exe"
+Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/p/?LinkID=869426&clcid=0xc09&culture=en-au&country=AU&lm=deeplink&lmsrc=groupChatMarketingPageWeb&cmpid=directDownloadWin64" -OutFile "C:\Sandbox\Teams.exe"
 Write-Output "Downloading ZoomIt, please wait"
-Invoke-WebRequest -Uri "https://live.sysinternals.com/ZoomIt64.exe" -OutFile "Data\Zoomit.exe"
+Invoke-WebRequest -Uri "https://live.sysinternals.com/ZoomIt64.exe" -OutFile "C:\Sandbox\Zoomit.exe"
 
-$usrPath = $env:USERPROFILE + "\Desktop\Windows-Sandbox-Presenter"
+$sandBox = "C:\Sandbox\TeamsDemo.wsb"
 
-$sandBox = "Data\TeamsDemo.wsb"
-[xml]$xmlDoc = Get-Content $sandBox
-#The WSB file is basically XML, I find the HostFolder tag and replace the generic %Userprofile% variable with one locally calculated
-$xmlDoc.SelectNodes("//HostFolder") | % { 
-    $_."#text" = $_."#text".Replace("%USERPROFILE%",$usrPath) 
-    }
-
-$xmlDoc.Save($usrPath + "\Data\TeamsDemo.wsb")
-Invoke-Item "Data\TeamsDemo.wsb"
+Invoke-Item $sandBox
